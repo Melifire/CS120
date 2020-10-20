6e0fdce4e62c4f16cc8b7edcb685a637a5aeb4a2
Ben Kruse <itsbenkruse@gmail.com>
2020-10-20 09:47:23 -0700

    graded commented

--- Projects/Long 8/annoying_recursion_part2.py
+++ Projects/Long 8/annoying_recursion_part2.py
@@ -1,18 +1,19 @@
 def annoying_valley(n):
+    # works for all cases except n == 1 and n == 0 which are the only needed base cases
     if n > 6:
         string_to_print = "." * (n-1)
         print(string_to_print + '/')
-        annoying_valley(n-1)
+        annoying_valley(n-1)  # recurion called enveloped by other code in the function
         print(string_to_print + '\\')
-    if n == 0:
+    if n == 0:  # base case: do nothing at 0
         pass
-    if n == 1:
+    if n == 1:  # base case: print the * at 1
         print('*')
-    if n == 2:
-        print('./\n*\n.\\')
+    if n == 2:  # at n == 2 and n == 3 outputs are hardcoded in accordance with the rules
+        print('./\n*\n.\\') 
     if n == 3:
         print('../\n./\n*\n.\\\n..\\')
-    if n == 4:
+    if n == 4:  # n == 4,5,6 are coppies of n > 6 with values hardcoded
         string_to_print = "." * (3)
         print(string_to_print + '/')
         annoying_valley(3)
@@ -29,19 +30,21 @@ def annoying_valley(n):
         print(string_to_print + '\\')
 
 def annoying_int_sequence(n):
-    if n > 6:
+    # recursive case works for all cases except and n == 0, 1 which are the only needed 
+    # base cases
+    if n > 6:  
         last_seq = annoying_int_sequence(n-1)
-        out_seq = (last_seq + [n]) * (n-1) + last_seq
+        out_seq = (last_seq + [n]) * (n-1) + last_seq 
         return out_seq
     if n == 0:
         return []
     if n == 1:
         return [1]
-    if n == 2:
+    if n == 2:  # n == 2, 3 hardcoded in accordance with the rules
         return [1, 2, 1]
     if n == 3:
         return [1, 2, 1, 3, 1, 2, 1, 3, 1, 2, 1]
-    if n == 4:
+    if n == 4:  # n == 4,5,6 are coppies of n > 6 with values hardcoded
         last_seq = annoying_int_sequence(3)
         out_seq = (last_seq + [4]) * (3) + last_seq
         return out_seq
--- Projects/Long 8/linked_list_recursion_part2.py
+++ Projects/Long 8/linked_list_recursion_part2.py
@@ -1,31 +1,44 @@
 from list_node import ListNode
 
 def array_to_list_recursive(data):
+    # if there are any elements in the array
     if data:
+        # created a list node with the current first value then sets its next value to 
+        # the node returned by this function called on the list without the first index
         node = ListNode(data[0])
         child_node = array_to_list_recursive(data[1:])
         node.next = child_node
         return node
     else:
+        # once there are no more items in the array, finish off the list with None
         return None
 
 def accordion_recursive(head):
+    # if the list has 0 or 1 element, return None
     if not head or not head.next:
         return None
     else:
+        # if the list has no third element, then the second value is returned: base case
         if not head.next.next:
             return head.next
         else:
+            # the method of calling recursion on the nodes that are going to be removed 
+            # is used because the first node must always be removed and it is also 
+            # always called
             in_three_nodes = head.next.next.next
-            accordion_recursive(head.next.next)
+            accordion_recursive(head.next.next)  # does not use the return value
             head = head.next
             head.next = in_three_nodes
-            return head
+            return head # return value is only used for calling from outside the function
 
 def pair_recursive(head1, head2):
+    #if either list is empty, we have reached the end and finish the list with None
     if head1 is None or head2 is None:
         return None
     else:
+        # makes a list node and sets its value to a tup of the values of each lists heads
         node = ListNode((head1.val, head2.val))
+        # sets its next node to the returned node of this function on the two lists 
+        # without thier heads
         node.next = pair_recursive(head1.next, head2.next)
         return node
