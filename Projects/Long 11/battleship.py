8bbce8d00f42efdaeade4d8b8b6f1cefed2df4e5
Ben Kruse <itsbenkruse@gmail.com>
2020-11-13 09:47:01 -0700

    battleship commented

--- Projects/Long 11/battleship.py
+++ Projects/Long 11/battleship.py
@@ -2,34 +2,44 @@ import math
 
 class Board():
     def __init__(self, size):
-        assert size > 0  # even though the spec says for >= 0, 0 wouldn't work
+        assert size > 0  # even though the spec says for >= 0, 0 doesn't work
         self._size = size
+        # a 2d array that represents the board. square with '.' in each position
         self._grid = [['.' for i in range(size)] for j in range(size)]
-        self._ships = []
-        self._taken_spaces = []
+        self._ships = []  # an array that holds all the ships
+        self._taken_spaces = []  # used to check for collisions
 
     def get_ships(self):
+        # returns the ship list
         return self._ships
 
     def add_ship(self, ship, position):
+        '''adds previously created Ship objects to the board class'''
         ship.set_position(position)
         for bit in ship.get_offset_shape():
+            # checks to make sure that each space in the new ship is not colliding with
+            # another ship and not off the board
             assert bit not in self._taken_spaces, 'Invalid Position'
             assert 0 <= bit[0] < self._size and 0 <= bit[1] < self._size
-            self._grid[bit[1]][bit[0]] = ship.get_name()[0]
-            self._taken_spaces.append(bit)
-        self._ships.append(ship)
+            self._grid[bit[1]][bit[0]] = ship.get_name()[0]  # to show ship on grid
+            self._taken_spaces.append(bit)  # for checking later collisions
+        self._ships.append(ship)  # adds the ship to the ship list
 
     def print(self):
-        num_len = int(math.log10(self._size))
-        print(' ' * (num_len+2) + '+' + '-' * (self._size*2 + 1) + '+')
+        '''using the boards '._grid', draw it out on the consol'''
+        num_len = int(math.log10(self._size))  # the character length of the size
+        # prints the top bound
+        print(' ' * (num_len+2) + '+' + '-' * (self._size*2 + 1) + '+') 
+        # prints each line of the board starting with the row number
         for i in range(self._size-1, -1, -1):
             print(' ' * (num_len - len(str(i)) + 1) + str(i) + ' | ', end='')
             for j in self._grid[i]:
                 print(j, end=' ')
             print('|')
-        print(' ' * (num_len+2) + '+' + '-' * (self._size*2 + 1) + '+')
-        for i in range(num_len+1):
+        print(' ' * (num_len+2) + '+' + '-' * (self._size*2 + 1) + '+') # bottom bound
+        # all of this is just used to correctly space the collumn numbers no matter how 
+        # many there are
+        for i in range(num_len+1): 
             print(' ' * (num_len+4), end='')
             for j in range(self._size):
                 if j < 10 ** (num_len - i) and i < num_len:
@@ -40,32 +50,37 @@ class Board():
             print()
             
     def has_been_used(self, position):
+        '''checks the board at a position to see if it has been used already'''
+        # if the board at any point is '*' 'o' or 'X' then the player has already shot 
         try_point = self._grid[position[1]][position[0]]
         if try_point == '*' or try_point == 'o' or try_point == 'X':
             return True
         return False
 
     def attempt_move(self, position):
+        '''attempts to make a move, assertion error if already tried. 
+        Returns 'Miss' 'Hit' or 'Sunk :(ship)' depending on the outcome
+        '''
         assert not self.has_been_used(position), 'Spot already chosen'
         shot_point = self._grid[position[1]][position[0]]
-        if shot_point == '.':
+        if shot_point == '.':  # no ships
             self._grid[position[1]][position[0]] = 'o'
             return 'Miss'
-        for ship in self._ships:
-            if position in ship.get_offset_shape():
+        for ship in self._ships:  # assumes that if it doesnt hit nothing, it hits a ship
+            if position in ship.get_offset_shape():  # if it hit this ship
                 hit_pos = ship.hit(position)
                 self._grid[hit_pos[1]][hit_pos[0]] = '*'
-                if ship.is_sunk():
+                if ship.is_sunk():  # checks it that was the final blow
                     for bit in ship.get_offset_shape():
                         self._grid[bit[1]][bit[0]] = 'X'
                     return f"Sunk ({ship.get_name()})"
                 return 'Hit'
-        return 'Something went worng'
+        return 'Something went worng' # This is for debugging and should never occur
 
 
 class Ship():
     def __init__(self, name, shape):
-        assert len(name) > 0
+        assert len(name) > 0  # later, name is indexed at 0 with no checks
         self._name = name
         self._shape = shape
         self._hits = [False for i in range(len(self._shape))]
@@ -75,6 +90,9 @@ class Ship():
         return self._name
 
     def print(self):
+        ''' prints the ship represented as a string of its components with a * for each
+        hit part. This is followed by the name of the ship
+        '''
         for i in range(len(self._shape)):
             if self._hits[i]:
                 print('*', end='')
@@ -87,6 +105,8 @@ class Ship():
         return self._shape
 
     def get_offset_shape(self):
+        '''Returns the offset shape which is the shape of the ship summed with the offet 
+            vector. Usefull for checking actual prosition on the board'''
         
         assert self._position, 'This piece has not yet been given a position'
         return ((self._position[0] + bit[0], self._position[1] + bit[1]) 
@@ -96,31 +116,36 @@ class Ship():
         self._position = position
 
     def hit(self, position):
+        ''' hits the ship at given position. 
+            The position is taken relative to the board'''
         un_offset_hit = (position[0] - self._position[0],
                          position[1] - self._position[1])
+        # makes sure that the hit point is valid
         assert un_offset_hit in self._shape, f"{un_offset_hit} - {self._shape}"
         i = self._shape.index(un_offset_hit)
         self._hits[i] = True
         bit = self._shape[i]
-        return [self._position[0] + bit[0], self._position[1] + bit[1]]
+        return [self._position[0] + bit[0], self._position[1] + bit[1]] 
 
     def is_sunk(self):
         return all(self._hits)
 
     def rotate(self, amount):
+        '''rotates the ship by dotting each part with a 2d rotation matrix'''
         assert 0 <= amount <= 3
         if amount == 0:
             return None
         elif amount == 1:
-            rotation_matrix = [[0, 1], [-1, 0]]
+            rotation_matrix = [[0, 1], [-1, 0]] # 90 degree rotation matrix
         elif amount == 2:
-            rotation_matrix = [[-1, 0], [0, -1]]
+            rotation_matrix = [[-1, 0], [0, -1]] # 180 degree rotation matrix
         elif amount == 3:
-            rotation_matrix = [[0, -1], [1, 0]]
+            rotation_matrix = [[0, -1], [1, 0]] # 270 degree rotation matrix
         for i, shape in enumerate(self._shape):
             self._shape[i] = self._math_dot_product(rotation_matrix, shape)
 
     def _math_dot_product(self, matrix, vector):
+        ''' does a dot product between a matrix and a vector '''
         assert matrix and vector, 'empty'
         assert len(vector) == len(matrix), 'dif len'
         out_vector = [0 for i in range(len(vector))]
--- Projects/Long 11/test-student-1.py
+++ Projects/Long 11/test-student-1.py
@@ -1,46 +1,58 @@
 import battleship
 
-game = battleship.Board(12)
 
-#must testcases be in a method
+def main():
+    game = battleship.Board(12)
 
-ship_list = [
-    (battleship.Ship('Nina', [(0,0), (1, 0), (2, 0), (3, 0)]), (3, 4), 0),
-    (battleship.Ship('Pinta', [(0,0), (1, 0), (2, 0), (3, 0)]), (0, 0), 0),
-    (battleship.Ship('Santa Maria', [(0,0), (1, 0), (2, 0), (3, 0)]), (0, 5), 1)
-]
+    # list of all ships to add
+    ship_list = [
+        (battleship.Ship('Nina', [(0,0), (1, 0), (2, 0), (3, 0)]), (3, 4), 0),
+        (battleship.Ship('Pinta', [(0,0), (1, 0), (2, 0), (3, 0)]), (0, 0), 0),
+        (battleship.Ship('Santa Maria', [(0,0), (1, 0), (2, 0), (3, 0)]), (0, 5), 1)
+    ]
 
-for ship in ship_list:
-    ship[0].rotate(ship[2])
-    game.add_ship(ship[0], ship[1])
+    # checks to makes sure rotation works
+    for ship in ship_list:
+        ship[0].rotate(ship[2])
+        game.add_ship(ship[0], ship[1])
 
-fail_ship_collide = battleship.Ship('Collide', [(0, 0)])
-try:
-    game.add_ship(fail_ship_collide, (3, 4))
-except:
-    print('Caught ship collision error')
+    # check colliding ships
+    fail_ship_collide = battleship.Ship('Collide', [(0, 0)])
+    try:
+        game.add_ship(fail_ship_collide, (3, 4))
+    except:
+        print('Caught ship collision error')
 
-fail_ship_bounds = battleship.Ship('Bounds', [(0, 0)])
-try:
-    game.add_ship(fail_ship_bounds, (100, 100))
-except:
-    print('Caught ship bounds error')
+    # check for out of bounds ship
+    fail_ship_bounds = battleship.Ship('Bounds', [(0, 0)])
+    try:
+        game.add_ship(fail_ship_bounds, (100, 100))
+    except:
+        print('Caught ship bounds error')
 
-for ship in game.get_ships():
-    ship.print()
+    # checks ship printing
+    for ship in game.get_ships():
+        ship.print()
 
-game.print()
+    # checks game printing and proper placement of ships
+    game.print()
 
-game.attempt_move((5, 5))
+    # checks miss
+    game.attempt_move((5, 5))
 
-game.print()
+    game.print()
 
-game.attempt_move((3, 4))
-game.attempt_move((4, 4))
-game.attempt_move((5, 4))
+    # checks hitting ship
+    game.attempt_move((3, 4))
+    game.attempt_move((4, 4))
+    game.attempt_move((5, 4))
 
-game.print()
+    game.print()
 
-game.attempt_move((6, 4))
+    #checks sinking ship
+    game.attempt_move((6, 4))
 
-game.print()
+    game.print()
+
+if __name__ == '__main__':
+    main()
